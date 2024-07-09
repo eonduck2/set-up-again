b5d9313f7eda1bd53210a5b715738573ac588e68
eonduck2 <whdtnxd@gmail.com>
2024-07-09 10:53:37 +0900

    test

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import ReactDOM from "react-dom";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("node:path");
 
 module.exports = {
-  entry: path.join(__dirname, "src", "app.tsx"),
+  entry: path.join(__dirname, "src", "index.tsx"),
   output: {
     filename: "app.bundle.js",
     path: path.resolve(__dirname, "dist"),
